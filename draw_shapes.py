d39132f4365229b6f4404fc132af66717cf8424e
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-02-10 14:20:35 -0800

    challenge26

--- draw_shapes.py
+++ draw_shapes.py
@@ -1,42 +1,34 @@
 # by Kami Bigdely
 # Extract superclass.
-class Circle:
-    
-    def __init__(self, x, y, r, visible = True):
-      self.center_x = x
-      self.center_y = y
-      self.r = r
-      self.visible = visible
-      
+class Shape:
+    def __init__(self, x, y, visible = True):
+        self.x = x
+        self.y = y
+        self.visible = visible
+
     def display(self):
-        print('drew the circle.')
-        
+        print('drew the shape.')
     def set_visible(self,is_visible):
         self.visible = is_visible
-        
+    def hide(self):
+        self.visible = False
+    def make_visible(self):
+        self.visible = True
+
+class Circle(Shape):
+    def __init__(self, x, y, r, visible = True):
+        super().__init__(x, y, visible)
+        self.r = r
+      
     def get_center(self):
         return self.center_x, self.center_y
     
     
 class Rectangle:
-    
     def __init__(self, x, y, width, height, visible = True):
-        # left-bottom corner.
-        self.x = x
-        self.y = y
+        super().__init__(x, y, visible)
         self.width = width
         self.height = height
-        self.visible = visible
-        
-    def display(self):
-        if self.visible:
-            print('drew the rectable.')
-            
-    def hide(self):
-        self.visible = False
-        
-    def make_visible(self):
-        self.visible = True
         
     def get_center(self):
         return self.x + self.width/2, \
